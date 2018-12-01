833245938235da93952e9590c9214defe77bb03d
Jack Chen <jackc0022@gmail.com>
2018-11-30 23:22:58 -0800

    removed useless files

--- iceCreamTruck/orders/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def validate_quantity(value):
-    if value > flavor.quantity_stocked:
-        raise ValidationError("Not enough product in stock")
-    return value
