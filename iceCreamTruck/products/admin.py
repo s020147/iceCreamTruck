b08492e17b177404bae01c6e0d7786b7e1592a9c
Jesus Aispuro <jesus10_an@hotmail.com>
2018-12-01 14:40:53 -0800

    fix price button to sort elements in list_display() - in admin.py - in products

--- iceCreamTruck/products/admin.py
+++ iceCreamTruck/products/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('flavor_name', 'flavor_id', '_price', 'quantity_stocked', 'date_created', 'date_updated',)
+    list_display = ('flavor_name', 'flavor_id', 'price', 'quantity_stocked', 'date_created', 'date_updated',)
     list_filter = ('date_created','date_updated', 'price',)
     
 admin.site.register(Product, ProductAdmin)
