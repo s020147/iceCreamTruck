0225ea729a95e5cc1a88a099345e4ed50f3cc4cb
Jack Chen <jackc0022@gmail.com>
2018-12-01 00:10:41 -0800

    order form updates product quantities on creation and deletion

--- iceCreamTruck/orders/models.py
+++ iceCreamTruck/orders/models.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 from customers.models import Customer
 from products.models import Product
 from django.core.exceptions import ValidationError
+from django.db.models.signals import pre_delete 
+from django.dispatch import receiver
 
 class OrderForm(models.Model):
     order_i_d = models.AutoField(primary_key=True)
@@ -15,9 +17,23 @@ class OrderForm(models.Model):
     date_updated = models.DateTimeField(auto_now = True)
     
     def clean(self):
-        from django.core.exceptions import ValidationError
         if self.quantity > self.flavor.quantity_stocked:
             raise ValidationError('Not enough product stocked.')
+            
+    def save(self, *args, **kwargs):
+        sold = self.flavor.quantity_stocked - self.quantity
+        self.flavor.quantity_stocked = sold
+        self.flavor.save()
+        super(OrderForm, self).save(*args, **kwargs)
+    
+    @receiver(pre_delete)
+    def delete_repo(sender, instance, **kwargs):
+        unsold = instance.flavor.quantity_stocked + instance.quantity
+        instance.flavor.quantity_stocked = unsold
+        instance.flavor.save()
+           
+    def __str__(self):
+        return '%s' % (str(self.order_i_d).zfill(6),)
     
     @property
     def _product_id(self):
