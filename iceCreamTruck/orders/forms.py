9199e6845e3b427e8e8f29a1e71e67667745a198
s020147 <tong.dustin@gmail.com>
2018-11-30 16:14:35 -0800

    Add files via upload
    additional database, customer, product form

--- iceCreamTruck/orders/forms.py
+++ iceCreamTruck/orders/forms.py
@@ -6,3 +6,9 @@ class OrderModelForm(forms.ModelForm):
     class Meta:
         model = OrderForm
         fields = '__all__'
+        exclude=('current_date',)
+
+class ProductModelForm(forms.ModelForm):
+    class Meta:
+        model = ProductForm
+        fields = '__all__'
