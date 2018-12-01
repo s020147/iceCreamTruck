c3891783dee93289d3faac4e16e5bea5cdf1d811
Jack Chen <jackc0022@gmail.com>
2018-11-30 16:54:04 -0800

    customer has state drop down menu

--- iceCreamTruck/customers/models.py
+++ iceCreamTruck/customers/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 class Customer(models.Model):   
+    state_list =(('AL','AL'), ('AK','AK'), ('AZ','AZ'), ('AR','AR'), ('CA','CA'), ('CO','CO'), ('CT','CT'), ('DC','DC'), ('DE','DE'), ('FL','FL'), ('GA','GA'), ('HI','HI'), ('ID','ID'), ('IL','IL'), ('IN','IN'), ('IA','IA'), ('KS','KS'), ('KY','KY'), ('LA','LA'), ('ME','ME'), ('MD','MD'), ('MD','MD'), ('MI','MI'), ('MN','MN'), ('MS','MS'), ('MO','MO'), ('MT','MT'), ('NE','NE'), ('NV','NV'), ('NH','NH'), ('NJ','NJ'), ('NM','NM'), ('NY','NY'), ('NC','NC'), ('ND','ND'), ('OH','OH'), ('OK','OK'), ('OR','OR'), ('PA','PA'), ('RI','RI'), ('SC','SC'), ('SD','SD'), ('TN','TN'), ('TX','TX'), ('UT','UT'), ('VT','VT'), ('VA','VA'), ('WA','WA'), ('WV','WV'), ('WI','WI'), ('WY','WY'))
     customer_id = models.AutoField(primary_key=True)
     first_Name = models.CharField(max_length = 25)
     last_Name = models.CharField(max_length = 25)
     street_Address = models.CharField(max_length = 50)
     city = models.CharField(max_length = 50)
-    state = models.CharField(max_length = 2, validators=[RegexValidator('.{2}$')])
+    state = models.CharField(max_length=2,choices=state_list,)
     zip_Code = models.CharField(max_length = 5, validators=[RegexValidator('^\d{5}$')])
     
     def __str__(self):
